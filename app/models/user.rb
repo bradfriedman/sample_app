bc91233eac97950c984ccc9c7e41310de0bdeacd
Brad Friedman <brad.friedman@gmail.com>
2012-02-06 16:39:01 -0500

    Finished Chapter 8 (Sign in and Sign out)

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  before_save :create_remember_token
   
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -20,4 +21,10 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }
             
   validates :password, presence: true
+  
+  private
+  
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
