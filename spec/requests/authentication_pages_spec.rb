0d587482eca48e6a7f8e8e1b323741bff0ebb574
Brad Friedman <brad.friedman@gmail.com>
2012-02-10 18:18:40 -0500

    Finished Exercises 1 and 2 from Chapter 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -40,6 +40,10 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+        it { should_not have_link('Users',          href: users_path) }
+        it { should_not have_link('Profile',        href: user_path(user)) }
+        it { should_not have_link('Settings',       href: edit_user_path(user)) }
+        it { should_not have_link('Sign out',       href: signout_path) }
       end
     end
   end
